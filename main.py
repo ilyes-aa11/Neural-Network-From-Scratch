ec0a6e018eef2ba3e7b2427a4a02f08a844e0646
ilyes-aa11 <ilyesoukil777@gmail.com>
2025-09-23 22:47:22 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
 import layer
 import activation
 import loss
-import optimizer
+import neuralnet
 import numpy as np
-from sklearn.datasets import load_digits # dataset for hand written digits a lighter version of the mnist dataset
-from sklearn.model_selection import train_test_split # used to split the dataset into training data & testing data
-from sklearn.metrics import accuracy_score # evaluates the final score of the network i.e the percentage of how well it performed
+from sklearn.datasets import load_digits 
+from sklearn.model_selection import train_test_split 
+from sklearn.metrics import accuracy_score 
 
-digits = load_digits()
-X , y = digits.data, digits.target
+X,y = load_digits(return_X_y=True)
 X = X / 16.0
 
 
@@ -25,22 +24,15 @@ layer1 = layer.DenseLayer(X.shape[1],32,activation.ReLU())
 layer2 = layer.DenseLayer(32,16,activation.ReLU())
 layer3 = layer.DenseLayer(16,10,activation.Softmax())
 
+net = neuralnet.NeuralNet(layer1,layer2,layer3,loss=loss.CategoricalLoss())
 
-lossFunction = loss.CategoricalLoss()
+
 # optimization
-optimizer.stochasticGradientDescent([layer1,layer2,layer3],lossFunction,X_train,y_train)
-
+net.fit(X_train,y_train)
 # testing
-layer1.forward(X_test)
-layer1.activate()
-layer2.forward(layer1.activated)
-layer2.activate()
-layer3.forward(layer2.activated)
-layer3.activate()
+y_pred = net.predict(X_test)
 
-print("Testing loss: ",lossFunction.forward(layer3.activated, y_test))
-
-y_pred = np.argmax(layer3.activated, axis=1)
+y_pred = np.argmax(y_pred, axis=1)
 y_test = np.argmax(y_test, axis=1) 
 
 print("accuracy: " ,accuracy_score(y_true=y_test,y_pred=y_pred)) # accuracy between 97% to 90%
