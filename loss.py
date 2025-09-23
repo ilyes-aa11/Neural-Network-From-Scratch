8dc4ba524ef820fd969798c4941975dbfd4972b2
ilyes-aa11 <ilyesoukil777@gmail.com>
2025-09-23 22:46:25 +0100

    Update loss.py

--- loss.py
+++ loss.py
@@ -5,22 +5,21 @@ class CategoricalLoss:
         self.name = "categorical"
     
     # returns the loss for the batch of outputs
-    def forward(self,y_pred,y_true,one_hot=True):
+    def calculate(self,y_pred,y_true):
         y_pred_clipped = np.clip(y_pred,1e-7,1-1e-7)
-        if one_hot:
-            samples_losses = -np.sum(y_true * np.log(y_pred_clipped), axis = 1)
-            
-            return np.mean(np.reshape(samples_losses,(-1)))
-
-        else:
-            samples_losses = y_pred_clipped[range(len(y_pred_clipped)), y_true]
-            return np.mean(-np.log(samples_losses))
+        samples_losses = -np.sum(y_true * np.log(y_pred_clipped), axis = 1)
+        return np.mean(np.reshape(samples_losses,(-1)))
+        
+    def calculate_derivative(self,y_pred,y_true):
+        y_pred_clipped = np.clip(y_pred,1e-7,1-1e-7)
+        return -y_true / y_pred_clipped
         
 class MeanSquared:
     def __init__(self):
         self.name = "meanSquared"
 
-    def forward(self,y_pred,y_true):
+    def calculate(self,y_pred,y_true):
         return (1 / y_true.size) * np.sum((y_pred - y_true) ** 2)
     
-
+    def calculate_derivative(self,y_pred,y_true) :
+        return (2 / y_true.size) * (y_pred - y_true)
